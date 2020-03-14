3206e289c0b84d81c1cebeb5a42043c05c82ad2d
Mohammed AbuAisha <mabuaisha@outlook.com>
2020-03-14 12:55:54 +0200

    Add more tools to setup in bastion node

--- modules/k8s/scripts/setup.sh
+++ modules/k8s/scripts/setup.sh
@@ -13,6 +13,8 @@ function setupVirtualenv(){
 
  source ~/.bashrc
 
+ exec $SHELL
+
  # Setup pyenv
  pyenv --version
  pyenv install 3.7.0
@@ -21,29 +23,47 @@ function setupVirtualenv(){
 }
 
 
-function installPackages(){
+function setupCommonPackages(){
  # install common development tool in centos machine
  sudo yum -y groupinstall "Development Tools"
  sudo yum -y install libffi-devel zlib-devel bzip2-devel readline-devel sqlite-devel wget curl llvm ncurses-devel openssl-devel lzma-sdk-devel libyaml-devel redhat-rpm-config
  sudo yum -y install git
+}
 
- cat <<EOF > /etc/yum.repos.d/kubernetes.repo
- [kubernetes]
- name=Kubernetes
- baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
- enabled=1
- gpgcheck=1
- repo_gpgcheck=1
- gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
- EOF
+function setupHelm(){
+ curl -sSLf https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+}
 
- sudo yum install -y kubectl
+function setupKubectl() {
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
+  kubectl version --client
+}
+
+function setupOpenfaas(){
+  curl -sL https://cli.openfaas.com -o faas.sh
+  chmod +x faas.sh && ./faas.sh
+  sudo cp faas-cli /usr/local/bin/faas-cli
+  sudo ln -sf /usr/local/bin/faas-cli /usr/local/bin/faas
+}
+
+function installPackages(){
+   setupCommonPackages
+
+   setupHelm
+
+   setupKubectl
+
+   setupOpenfaas
 
 }
 
 function prepareKubespray(){
-  git clone git@github.com:kubernetes-sigs/kubespray.git
+  git clone https://github.com/kubernetes-sigs/kubespray.git
   cd kubespray
+
+  source ~/.bashrc
 
   pyenv activate k8s
 
@@ -51,7 +71,14 @@ function prepareKubespray(){
 
   pyenv deactivate
 
+}
 
+function setupK8SCluster(){
+  echo "Run cluster"
+}
+
+function deployOpenfaas(){
+ echo "Run OpenFaas"
 }
 
 # Install Packages
@@ -63,3 +90,8 @@ setupVirtualenv
 # Prepare Kubespary
 prepareKubespray
 
+# Setup cluster
+setupK8SCluster
+
+# Deploy OpenFaas
+deployOpenfaas
