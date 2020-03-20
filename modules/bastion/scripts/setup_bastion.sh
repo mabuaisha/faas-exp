1e89f7c7b0276aa14c17f4340bfe93fc3f34af48
Mohammed AbuAisha <mabuaisha@outlook.com>
2020-03-20 14:24:48 +0200

    Update bastion setup script

--- modules/bastion/scripts/setup_bastion.sh
+++ modules/bastion/scripts/setup_bastion.sh
@@ -8,7 +8,7 @@ echo "Installing Docker..."
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2 git
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce-19.03.7-3.el7
-sudo yum install httpd-tools
+sudo yum install -y httpd-tools
 
 # Config docker
 sudo usermod -aG docker centos
