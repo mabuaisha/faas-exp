7fd1d0aea8dbb769ee7bd669994f5383d4a390a3
Mohammed AbuAisha <mabuaisha@outlook.com>
2020-03-21 13:39:19 +0200

    Update function for nodejs

--- functions/computation-scenarios/matrix/matrixfunction/handler.js
+++ functions/computation-scenarios/matrix/matrixfunction/handler.js
@@ -5,17 +5,22 @@ const Matrix = require("./matrix.js");
 module.exports = (event, context) => {
   console.log(event);
   console.log(context);
-  // const param = event["queryStringParameters"]['param'] || 1;
-  // console.log('Input param=' + param);
-  // const a = Matrix.create(param, param);
-  // const b = Matrix.create(param, param);
-  // const resultBig = Matrix.multiply(a, b);
-  // const result = {
-  //   'result': Matrix.subset(resultBig, 0, 0, 10, 10)
-  // };
+  let param = 1;
+  const isExist = 'param' in event['query'];
+  if (isExist) {
+      param = event['query']['param']
+
+  }
+  console.log('Input param=' + param);
+  const a = Matrix.create(param, param);
+  const b = Matrix.create(param, param);
+  const resultBig = Matrix.multiply(a, b);
+  const result = {
+    'result': Matrix.subset(resultBig, 0, 0, 10, 10)
+  };
 
   return context
     .status(200)
-    .succeed({'result': 'Test'})
+    .succeed(result)
 }
 
