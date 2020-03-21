6c422b9dbd46b45809c24f52b3f42e4803aa47f3
Mohammed AbuAisha <mabuaisha@outlook.com>
2020-03-21 12:15:05 +0200

    Add matrix function

---
+++ functions/computation-scenarios/matrix/matrixfunction/handler.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Matrix = require("./matrix.js");
+
+module.exports = (event, context) => {
+  console.log(event);
+  console.log(context);
+  const param = event["queryStringParameters"]['param'] || 1;
+  console.log('Input param=' + param);
+  const a = Matrix.create(param, param);
+  const b = Matrix.create(param, param);
+  const resultBig = Matrix.multiply(a, b);
+  const result = Matrix.subset(resultBig, 0, 0, 10, 10);
+
+  return context
+    .status(200)
+    .succeed(result)
+}
+
