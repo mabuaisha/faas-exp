7714140138415ccf87ce4e276664e24e88f00de7
Mohammed AbuAisha <mabuaisha@outlook.com>
2020-03-23 21:51:10 +0200

    Update consumer function

--- functions/composite-scenarios/consumer/consumerfunction/handler.js
+++ functions/composite-scenarios/consumer/consumerfunction/handler.js
@@ -1,19 +1,30 @@
 'use strict'
+const request = require("request");
 
 module.exports = (event, context) => {
-  const gateway = process.env.gateway_endpoint
-
   console.log(event);
   console.log(context);
+  const gateway_endpoint = process.env.gateway_endpoint;
+
+  if (!gateway) {
+      return context.status.fail('Gateway URL is missing')
+  }
   let param = 1;
   const isExist = 'param' in event['query'];
   if (isExist) {
       param = event['query']['param']
 
   }
+  // This call to a matrix function
+  const url = gateway_endpoint + "/function/matrixfunction?param=" + param;
+  let result = '';
+  request.get(url, (error, response, body) => {
+    result = JSON.parse(body);
+    console.log(result);
+  });
 
   return context
     .status(200)
-    .succeed({'environment_variable': gateway, 'param': param})
+    .succeed({'environment_variable': gateway_endpoint, 'param': param, 'response': result})
 }
 
