4f612f8ca1a4c4db9c00eb59f713d1b10268eb15
Mohammed AbuAisha <mabuaisha@outlook.com>
2020-03-23 21:32:10 +0200

    Update functions

--- functions/composite-scenarios/consumer/consumerfunction/handler.js
+++ functions/composite-scenarios/consumer/consumerfunction/handler.js
@@ -3,8 +3,17 @@
 module.exports = (event, context) => {
   const gateway = process.env.gateway_endpoint
 
+  console.log(event);
+  console.log(context);
+  let param = 1;
+  const isExist = 'param' in event['query'];
+  if (isExist) {
+      param = event['query']['param']
+
+  }
+
   return context
     .status(200)
-    .succeed({'environment_variable': gateway})
+    .succeed({'environment_variable': gateway, 'param': param})
 }
 
